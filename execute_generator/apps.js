547727cb10f056eca075fea12eae3b1dab71125c
rvpacheco <rvpacheco17@gmail.com>
2023-03-09 01:50:14 +0000

    code improve

--- execute_generator/apps.js
+++ execute_generator/apps.js
@@ -1,16 +1,22 @@
-let who = ['My dog', 'My grandma', 'My boss', 'My neighbor'];
-let action = ['ate', 'broke', 'peed on', 'hid'];
-let what = ['my homework', 'the keys', 'the car'];
-let when = ['before I got home', 'during the call', 'right at that moment', 'while cooking', 'while doing homework'];
 
 
+const who = ['My dog', 'My grandma', 'My boss', 'My neighbor'];
+const action = ['ate', 'broke', 'peed on', 'hid'];
+const what = ['my homework', 'the keys', 'the car'];
+const when = ['before I got home', 'during the call', 'right at that moment', 'while cooking', 'while doing homework'];
+
+// Genera un número aleatorio entre min (inclusive) y max (exclusivo)
+function generarNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Genera una excusa aleatoria y la muestra en el elemento con id "excuse"
 function generarExcusa() {
-  let randomWho = who[Math.floor(Math.random() * who.length)];
-  let randomAction = action[Math.floor(Math.random() * action.length)];
-  let randomWhat = what[Math.floor(Math.random() * what.length)];
-  let randomWhen = when[Math.floor(Math.random() * when.length)];
-  
+  const randomWho = who[generarNumeroAleatorio(0, who.length)];
+  const randomAction = action[generarNumeroAleatorio(0, action.length)];
+  const randomWhat = what[generarNumeroAleatorio(0, what.length)];
+  const randomWhen = when[generarNumeroAleatorio(0, when.length)];
 
-  let excusa = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen + ".";
+  const excusa = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
   document.getElementById("excuse").innerHTML = excusa;
 }
